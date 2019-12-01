5c7d45aba2a7626532296c302edcbeb90f63b29d
robertkirkpatrick <rk3505@nyu.edu>
2019-12-01 12:36:30 -0500

    corrected main to accept arguments from command and use them to open files. free'd the misspelled array.

--- main.c
+++ main.c
@@ -5,16 +5,23 @@
 #define DICTIONARY "wordlist.txt"
 #define TESTDICT "test_worlist.txt"
 
-int main() {
+int main(int argc, char **argv) {
     hashmap_t hashtable[HASH_SIZE];
+    char *misspelled[MAX_MISSPELLED];
+    int i;
+    char *filetest = argv[1];
+    char *wordlist =argv[2];
+    FILE* fp = fopen(filetest, "r");
+
     //int num_misspelled = check_words(text_file, hashtable, misspelled);
-    load_dictionary("/home/rob/appsecurity/app-security/wordlist.txt", hashtable);
+    load_dictionary(wordlist, hashtable);
     //printf("%d\n", success);
     //char *word2 = "semicolons";
     //bool test = check_word(word2, hashtable);
     //printf("%d\n", test);
-    char *misspelled[MAX_MISSPELLED];
-    FILE* fp = fopen("/home/rob/appsecurity/app-security/test1.txt", "r");
+
+
+
     int miss = check_words(fp, hashtable, misspelled);
     printf("%d\n", miss);
     //FILE* fp2 = fopen("/home/rob/appsecurity/project1/a_tale_of_two_cities.txt", "r");
@@ -24,5 +31,10 @@ int main() {
     //check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[])
     //fclose(fp2);
     fclose(fp);
+    for(i = 0; i< MAX_MISSPELLED; i++){
+        if(misspelled[i] !=NULL){
+            free(misspelled[i]);
+        }
+    }
     return 0;
 }
